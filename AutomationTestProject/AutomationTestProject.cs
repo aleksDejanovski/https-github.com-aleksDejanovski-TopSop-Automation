1d4b85b762bf6e5e2e185eea56a1e8fe142071f4
aleksandar dejanovsi <dejanovski_a@yahoo.com>
2023-01-08 14:55:01 +0100

    Add project files.

---
+++ AutomationTestProject/AutomationTestProject.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AutomationTestProject
+{
+    [TestFixture]
+    public class TestCases
+    {
+        IWebDriver driver;
+        string webpage = "https://www.topshop.com.mk/";
+        Homepage page;
+
+        [SetUp]
+        public void setiranje()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(webpage);
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            page = new Homepage(driver);
+        }
+        [TearDown]
+        public void tearDown()
+        {
+            driver.Quit();
+            driver.Dispose();
+        }
+        [Test]
+        public void frontPageElements()
+        {
+            page.DomacinstvoKlik();
+            Assert.IsTrue(page.TitleDomacinstvo.Text.Contains("Домаќинство"));
+        }
+    }
+
+}
---
+++ AutomationTestProject/Homepage.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+
+namespace AutomationTestProject
+{
+    internal class Homepage
+    {
+        private IWebDriver driver;
+
+        public Homepage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+        //element domacinstvo
+        public IWebElement Domacinstvo => driver.FindElement(By.XPath("//span[contains(text(),'Домаќинство')]"));
+
+        public IWebElement TitleDomacinstvo => driver.FindElement(By.CssSelector("div[class='page-title category-title clearfix'] h1"));
+
+        public void DomacinstvoKlik()
+        {
+            Domacinstvo.Click();
+        }
+
+    }
+}
